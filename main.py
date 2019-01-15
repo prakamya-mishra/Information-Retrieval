b3a1fe1f1cb39cf273071cf35c71553215141c65
prakamya-mishra <pm669@snu.edu.in>
2019-01-15 13:24:25 +0530

    Initialized the project

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+
+documentList = []
+documentsProcessedWords = {}
+documentCount = 0
+for file in os.listdir("./dataset"):
+    documentCount = documentCount + 1
+    if file.endswith(".txt"):
+        documentList.append(str(file))
+        with open('dataset/'+file, 'r') as myfile:
+            data = myfile.read()
+            words = word_tokenize(data)
+            for w in words:
+                if not documentsProcessedWords.has_key(w):
+                    documentsProcessedWords[w] = {str(documentCount)}
+                else:
+                    documentsProcessedWords[w].add(str(documentCount))
+print documentsProcessedWords
+                
+
