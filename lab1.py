50d3a728800c96b32223db329b03e324a70e435d
Arpan Jain <arpanjain97@gmail.com>
2019-01-20 18:09:38 +0000

    [Refactor] Combine main.py and indexer.py into lab1.py

--- lab1.py
+++ lab1.py
@@ -1,6 +1,45 @@
-import os
-import indexer
+import os, click, nltk
 from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def createCSVFile():
+    # Initialize Data Structures
+    documentList = []
+    invertedIndex = {}
+    documentId = -1
+
+    # Initialize Stemmer
+    stemmer = PorterStemmer()
+
+    # Iterating all files in 'dataset' directory
+    documents = [ document for document in os.listdir('./dataset') if document.endswith('.txt') ]
+    with click.progressbar(documents) as docs:
+        for file in docs:
+            documentId += 1
+            documentList.append(file.replace('.txt', ''))
+            # New Syntax: removes the need to write cleanup code ie. close() after open()
+            with open('dataset/'+file, 'r') as inputFile:
+                data = inputFile.read()
+                words = word_tokenize(data)
+                # Adding stemmed words into inverted index
+                for word in words:
+                    stemmedWord = stemmer.stem(word).lower()
+                    if stemmedWord not in invertedIndex:
+                        invertedIndex[stemmedWord] = { documentId }
+                    elif documentId not in invertedIndex[stemmedWord]:
+                        invertedIndex[stemmedWord].add(documentId)
+
+    # Save as CSV
+    with open('inverted-index.csv', 'w') as outputFile:
+        for word in invertedIndex:
+            sortedDocIds = sorted(invertedIndex[word])
+            for i in range(len(sortedDocIds)):
+                sortedDocIds[i] = str(sortedDocIds[i])
+            indexRow = word + ',' + ','.join(sortedDocIds) + '\n'
+            outputFile.write(indexRow)
+    # Save Document Id List
+    with open('document-list.txt', 'w') as outputFile:
+        outputFile.write('\n'.join(documentList))
 
 # Initialize Stemmer
 stemmer = PorterStemmer()
@@ -9,7 +48,7 @@ stemmer = PorterStemmer()
 path_dir = os.path.abspath('.')
 if not os.path.exists(os.path.join(path_dir, 'inverted-index.csv')):
     print('inverted-index.csv not found...\nGenerating one for you!')
-    indexer.createCSVFile()
+    createCSVFile()
     print('Required files generated!')
 
 # Parsing document-list.txt
