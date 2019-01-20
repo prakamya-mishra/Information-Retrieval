1f5be80a6c312b12f390dae73aebc8e9b871a94d
Arpan Jain <arpanjain97@gmail.com>
2019-01-20 17:49:16 +0000

    [Chore] Modify run.bash to execute any python file in directory

--- run.bash
+++ run.bash
@@ -3,5 +3,10 @@
 source ENV/bin/activate
 pipreqs --force . --ignore ENV 
 pip install -r requirements.txt
-python main.py
+if [ -z "$1" ]
+  then
+    echo -e "\e[0;31mNo File Selected.\e[0m \nCommand should be of format: \e[0;32m/path/to/run.bash <file>\e[0m"
+  else
+    python $1
+fi
 deactivate
